dd7206bc87b27db6b4603717d067a702aa624c15
Mohit <msair107@gmail.com>
2024-02-16 01:39:28 +0530

    valid parenthesis

---
+++ Python/valid parenthesis.py
@@ -0,0 +1,25 @@
+"""
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+def isValid(s):
+    mapping = {"(":")","{":"}","[":"]"}
+    stack = []
+    for char in s:
+        if char in ["(","{","["]:
+            stack.append(mapping[char])
+        elif len(stack) == 0 or stack.pop() != char:
+            return False
+    return not len(stack) != 0
+
+s = "(}"
+
+print(isValid(s))
+
+
